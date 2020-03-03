bc27a81ba8cc6732fe29e8c615b24d1c64f92d59
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 17:03:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,6 +16,18 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
+  
+  collection_index = 0
+
+  while collection_index < collection.size do
+    current_item = collection[collection_index]
+    if ( current_item[:item] == name )
+      return current_item
+    end
+    collection_index += 1
+  end
+  nil
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
