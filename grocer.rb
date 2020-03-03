6fc0803a17d714d140a41c967395e5e3420a66cb
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 17:03:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,6 +57,24 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
+  
+  coupons_index = 0
+
+  while coupons_index < coupons.size do
+    current_coupon = coupons[coupons_index]
+    applicable_for_discount = find_item_by_name_in_collection( current_coupon[:item], cart )
+      if ( applicable_for_discount[:count] / current_coupon[:num] >= 1 )
+        cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
+                    :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
+                    :clearance => applicable_for_discount[:clearance],
+                    :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % current_coupon[:num])})
+
+        applicable_for_discount[:count] %= current_coupon[:num]
+      end
+    coupons_index += 1
+  end
+  cart
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
