63dac4ebac9e2a14d6ccd2c2427897c6d3f105d6
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 17:03:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,6 +35,22 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  
+  updated_cart = Array.new
+  cart_index = 0
+
+  while cart_index < cart.size do
+    current_item = cart[cart_index]
+    if ( find_item_by_name_in_collection( current_item[:item], updated_cart ) == nil)
+      current_item[:count] = 1
+      updated_cart.push(current_item)
+    else
+      increment_count_of_item( updated_cart, current_item[:item] )
+    end
+    cart_index += 1
+  end
+  updated_cart
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
